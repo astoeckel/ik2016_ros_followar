5aebc6207c8199b0f245ccf77dd3ac51f305db9d
robot <robot@robot7.kbs>
2016-03-06 23:42:44 +0100

    Main followar script

---
+++ python/followar.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import rospy
+import numpy as np
+
+from ar_pose.msg import ARMarker
+from geometry_msgs.msg import Twist
+
+
+import math
+
+
+def computeSpeed(dist):
+    v_max = 1.0  # m/s
+    dist_min = 0.1  # m
+    dist_max = 2.0  # m
+    if dist < dist_min:
+        return 0.0
+    else:
+        return v_max * min(1.0, dist / dist_max)
+
+
+class FollowarController:
+
+    def __init__(self):
+        self.sub = rospy.Subscriber("ar_pose_marker", ARMarker, self.callback)
+        self.pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+
+    def callback(self, marker):
+        # Fetch the marker pose
+        pose = marker.pose.pose
+
+        # Convert the position to a numpy vector, calculate the angle
+        pos = np.array([pose.position.x, pose.position.y, pose.position.z])
+        marker_angle = math.asin(pos[0] / np.linalg.norm(pos))
+
+        # Turn the robot such that the angle gets closer to zero
+        t = Twist()
+        t.linear.x = computeSpeed(pos[2])
+        t.angular.z = -np.sign(marker_angle) * max(
+            abs(marker_angle) * 10, math.pi)
+
+        self.pub.publish(t)
+
+
+if __name__ == '__main__':
+    rospy.init_node('followar')
+    controller = FollowarController()
+    rospy.spin()
+
