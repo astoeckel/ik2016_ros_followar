91e41bc1ef1d10f4f72d6b1f5405a5954b70cd5d
IK standard user <ik@guenne.ik>
2016-03-09 23:49:28 +0100

    use camera transformation

--- python/followar.py
+++ python/followar.py
@@ -2,9 +2,10 @@
 
 import rospy
 import numpy as np
+import tf
 
 from ar_pose.msg import ARMarker
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, PoseStamped
 
 
 import math
@@ -25,20 +26,28 @@ class FollowarController:
     def __init__(self):
         self.sub = rospy.Subscriber("ar_pose_marker", ARMarker, self.callback)
         self.pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+        self.tfl = tf.TransformListener()
 
     def callback(self, marker):
-        # Fetch the marker pose
-        pose = marker.pose.pose
+        # Build a PoseStamped from the marker
+        try:
+            self.tfl.waitForTransform(marker.header.frame_id, "base_link",
+                marker.header.stamp, rospy.Duration(0.1))
+            pose = PoseStamped(header=marker.header, pose=marker.pose.pose)
+            pose = self.tfl.transformPose("base_link", pose).pose
+        except Exception as e:
+            rospy.logwarn(str(e))
+            return
 
         # Convert the position to a numpy vector, calculate the angle
         pos = np.array([pose.position.x, pose.position.y, pose.position.z])
-        marker_angle = math.asin(pos[0] / np.linalg.norm(pos))
+        marker_angle = math.atan2(pos[1], pos[0])
 
         # Turn the robot such that the angle gets closer to zero
         t = Twist()
-        t.linear.x = computeSpeed(pos[2])
-        t.angular.z = -np.sign(marker_angle) * min(
-            abs(marker_angle) * 2, math.pi)
+        t.linear.x = computeSpeed(math.hypot(pos[0], pos[1]))
+        t.angular.z = np.sign(marker_angle) * min(
+            abs(marker_angle) * 4, math.pi)
 
         self.pub.publish(t)
 
